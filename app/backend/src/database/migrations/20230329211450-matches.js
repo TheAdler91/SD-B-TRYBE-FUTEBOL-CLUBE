25de90609048008082970f799c069c26eab52f6f
Your NamePhilip <philip.adler@hotmail.com>
2023-03-29 18:30:13 -0300

    Feat: Match's Migration and Model created

---
+++ app/backend/src/database/migrations/20230329211450-matches.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("matches", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      awayTeamId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+        field: "away_team_id",
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: "away_team_goals",
+      },
+      homeTeamId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+        field: "home_team_id",
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: "home_team_goals",
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        field: "in_progress",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'Matches',
+  timestamps: false,
+});
