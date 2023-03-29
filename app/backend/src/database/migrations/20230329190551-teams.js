533176d2cc696d9c28e25face880779005675806
Your NamePhilip <philip.adler@hotmail.com>
2023-03-29 16:42:58 -0300

    Feat: Team's Migration and Model created

---
+++ app/backend/src/database/migrations/20230329190551-teams.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'team_name',
+      },
+    });
+  },
+
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
